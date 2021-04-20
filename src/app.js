06382f7114892241a136c72946b5dc76bf5c1202
Abiodun <abiodundev@gmail.com>
2021-04-20 10:46:25 +0100

    creating a simle express application

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 2022;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express application Loading...</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`The Server is running on PORT ${port}`);
+});
